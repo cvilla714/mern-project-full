df54a58d05c3206afe3065ae8117ba638e94e1d8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-12 21:03:52 -0400

    added dotnev and changed the token variable

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,40 +1,41 @@
 const jtw = require('jsonwebtoken');
 const UserModel = require('../config/models/user.model');
+require('dotenv').config();
+let token_secret = process.env.TOKEN_SECRET;
 
 module.exports.ckeckUser = (req, res, next) => {
-    const token = req.cookies.jtw;
-    if (token) {
-        jtw.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
-            if (err) {
-                res.locals.user = null;
-                res.cookie('jtw', '', { maxAge : 1 })
-                next();
-            } else {
-               let user = await UserModel.findById(decodedToken.id);
-                res.locals.user = user;
-
-                next();
-            } 
-
-        })
-    } else {
+  const token = req.cookies.jtw;
+  if (token) {
+    jtw.verify(token, token_secret, async (err, decodedToken) => {
+      if (err) {
         res.locals.user = null;
+        res.cookie('jtw', '', { maxAge: 1 });
         next();
-    }
+      } else {
+        let user = await UserModel.findById(decodedToken.id);
+        res.locals.user = user;
+
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
 };
 
 module.exports.requireAuth = (req, res, next) => {
-    const token = req.cookies.jtw;
-    if (token) {
-        jtw.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(decodedToken)
-                next();
-            }
-        })
-    } else {
-        console.log('No token')
-    }
-}
+  const token = req.cookies.jtw;
+  if (token) {
+    jtw.verify(token, token_secret, async (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    console.log('No token');
+  }
+};
