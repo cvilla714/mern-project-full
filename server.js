02da82de2ffa6d0cf32d95d757348cc70ebd380c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-12 21:01:41 -0400

    added express.json instead of deprecated body parser, fixed node enviroment

--- server.js
+++ server.js
@@ -1,65 +1,56 @@
 const express = require('express');
+const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes')
-const connectDB = require('./config/db')
+const postRoutes = require('./routes/post.routes');
+const connectDB = require('./config/db');
 
-require('dotenv').config({path: './config/.env'});
-require('./config/db');
+// require('dotenv').config({ path: './config/.env' });
+// require('./config/db');
 
-connectDB()
+connectDB();
 
-
-const {ckeckUser, requireAuth} = require('./middleware/auth.middleware');
+const { ckeckUser, requireAuth } = require('./middleware/auth.middleware');
 const cors = require('cors');
 
-const app = express();
-
 const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    Credentials: true,
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'prefFlightContinue': false
-    
-
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ['sessionId', 'Content-Type'],
+  exposedHeaders: ['sessionId'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  prefFlightContinue: false,
 };
 
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions))
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
 //jtw
 app.get('*', ckeckUser);
 app.get('/jtwid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
-})
+  res.status(200).send(res.locals.user._id);
+});
 
 //routes
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 
-
 //server
-if(process.env.MODE_ENV === "production") {
-   app.use(express.static(path.join(__dirname, "/client/build")))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/client/build'));
 
-   app.get('*', (req, res) => {
-       res.sendFile(path.join(__dirname, "client" , "build" , "index.html"));
-   })
-} else {
-    app.get('/', (req, res) => {
-        res.send("Api running")
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
